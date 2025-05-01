de6ebcf9391510ccccbfb3c142a7b4cba00ba092
LinMarc1210 <marc1210899@gmail.com>
2025-05-01 16:01:59 +0800

    Refactor: remove redundant commments

--- realsense/coor_reconstruct.py
+++ realsense/coor_reconstruct.py
@@ -4,22 +4,16 @@ import pandas as pd
 import os
 import open3d as o3d
 
-# 設定 .bag 檔案的路徑
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 bag_file = os.path.join(BASE_DIR, 'bags', '20250311_140524.bag')
-
-# 讀取 CSV 檔案
-csv_file = os.path.join(BASE_DIR, 'pointclouds', 'pointcloud_20250419_202146.csv')  # 用你的 CSV 檔案路徑
+csv_file = os.path.join(BASE_DIR, 'pointclouds', 'pointcloud_20250419_202146.csv')
 df = pd.read_csv(csv_file)
-
-# 設定參考點
 referencePoints_pixelDepth = [
     [475, 83, 691],
     [958, 130, 638],
     [330, 621, 551],
     [1395, 648, 577]
 ]
-
 referencePoints_realWorld = np.array([
     [0.002, 0.3, 0.0, 1.0],
     [0.2468, 0.2415, 0.033, 1.0],
@@ -32,7 +26,6 @@ class ImageAndDepth2RealWorldTransformator:
         self.intrinsics = intrinsics
         np_pixelDepth = np.array(referencePoints_pixelDepth)
 
-        # Assert 檢查
         assert np_pixelDepth.shape[0] >= 4, "Array must have at least 4 rows"
         assert np_pixelDepth.shape[1] == 3, "Array must have exactly 3 columns"
         assert referencePoints_realWorld.shape[0] == np_pixelDepth.shape[0], "Arrays must have the same number of rows"
@@ -73,63 +66,43 @@ class ImageAndDepth2RealWorldTransformator:
         vPoint[0, 0:3] = point
         return np.dot(self.transformationMatrixImage2RealWorld, np.array(vPoint[0]))[0:3]
 
-
-# 開啟 .bag 檔案
 pipeline = rs.pipeline()
 config = rs.config()
-
-config.enable_device_from_file(bag_file)  # 用你的 .bag 檔案路徑
+config.enable_device_from_file(bag_file)
 pipeline.start(config)
 
-# 取得深度影像的內部參數
 profile = pipeline.get_active_profile()
 depth_profile = rs.video_stream_profile(profile.get_stream(rs.stream.depth))
 color_profile = rs.video_stream_profile(profile.get_stream(rs.stream.color))
 depth_intrinsics = depth_profile.get_intrinsics()
 color_intrinsics = color_profile.get_intrinsics()
 
-# 初始化變換器，並傳入相機內參和參考點
 imageAndDepth2RealWorldTransformator = ImageAndDepth2RealWorldTransformator(color_intrinsics, referencePoints_pixelDepth, referencePoints_realWorld)
 
-# 創建 Open3D 點雲物件
 point_cloud = o3d.geometry.PointCloud()
 
-# 用來儲存點雲的 NumPy 陣列
 points_list = []
 colors_list = []
-
-# 讀取 CSV 檔案並處理每一行
 for index, row in df.iterrows():
-    # 取得 CSV 中的 u, v, z (z*100)
     u = row['u']
     v = row['v']
-    z = row['z'] * 100  # 這裡乘以 100 是你提到的要求
+    z = row['z'] * 100
     R = row['R']
     G = row['G']
     B = row['B']
 
-    # 將每個點的 [u, v, z*100] 當作測試點
     result = imageAndDepth2RealWorldTransformator.pixelDepth2RealWorld(u, v, z)
     # print(f"Test: {u}, {v}, {z}")
     # print("Result:", result)
 
-    # 設置每個點的顏色 (RGB)
-    color = np.array([B / 255.0, G / 255.0, R / 255.0])  # 顏色範圍 [0, 1], BGR -> RGB
-    
-    # 將 3D 點和顏色分別加入列表中
+    color = np.array([B / 255.0, G / 255.0, R / 255.0])
     points_list.append(result)
     colors_list.append(color)
 
-# 將點和顏色轉換為 NumPy 陣列
 points_array = np.array(points_list)
 colors_array = np.array(colors_list)
-
-# 將點雲的點和顏色設置到 Open3D 點雲物件中
 point_cloud.points = o3d.utility.Vector3dVector(points_array)
 point_cloud.colors = o3d.utility.Vector3dVector(colors_array)
-
-# 顯示 3D 點雲
 o3d.visualization.draw_geometries([point_cloud])
 
-# 停止管道
 pipeline.stop()
