49186a2be607d43a78edce50247643ac48314ced
魯倫愷 <ja0741187@gmail.com>
2025-06-13 00:25:22 +0800

    app config modified

--- demo_web/__init__.py
+++ demo_web/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from datetime import timedelta
 
 
 app = Flask(__name__)
 app.config.from_object('demo_web.config.Config')
+
 db = SQLAlchemy(app)
 
 from demo_web.models import db, User 
