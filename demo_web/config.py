fa5546a5007f2a84b28534c23cda035916ce75f1
魯倫愷 <ja0741187@gmail.com>
2025-06-13 00:26:12 +0800

    app config key modified to fixed key

--- demo_web/config.py
+++ demo_web/config.py
@@ -4,6 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class Config:
-    SECRET_KEY = secrets.token_hex(16)
+    #SECRET_KEY = secrets.token_hex(16)
+    SECRET_KEY = 'your-fixed-secret-key-here-for-development'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
